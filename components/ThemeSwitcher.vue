2439f092e26b51b530d5c7152c129cf15b3a93bc
Elijah Ellanski <ilya.ellanskiy@itms.tech>
2025-02-28 13:31:43 +0300

    messing around theming

--- components/ThemeSwitcher.vue
+++ components/ThemeSwitcher.vue
@@ -1,29 +1,27 @@
+<script setup lang="ts">
+const color = useColorMode()
+
+useHead({
+  meta: [{
+    id: 'theme-color',
+    name: 'theme-color',
+    content: () => color.value === 'dark' ? '#222222' : '#ffffff',
+  }],
+})
+
+function toggleTheme() {
+  color.preference = color.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
 <template>
   <div class="flex items-center gap-2">
     <UButton
-      v-for="theme in ['light', 'dark', 'system']"
-      :key="theme"
-      :icon="getThemeIcon(theme)"
+      :icon="color.value === 'dark' ? 'i-carbon-sun' : 'i-carbon-moon'"
       color="gray"
       variant="ghost"
-      :class="{ 'text-primary': $colorMode.preference === theme }"
-      @click="$colorMode.preference = theme"
-      :aria-label="`Turn on ${theme} theme`"
+      @click="toggleTheme"
+      :aria-label="`Turn off ${color.preference} theme`"
     />
   </div>
 </template>
-
-<script setup lang="ts">
-const getThemeIcon = (theme: string) => {
-  switch (theme) {
-    case 'light':
-      return 'i-carbon-sun'
-    case 'dark':
-      return 'i-carbon-moon'
-    case 'system':
-      return 'i-carbon-screen'
-    default:
-      return ''
-  }
-}
-</script>
