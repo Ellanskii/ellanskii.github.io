704029498cc72770595fc6be002a731dae8c5767
Elijah Ellanski <ilya.ellanskiy@itms.tech>
2025-02-28 12:29:23 +0300

    add article page

--- pages/articles/[...slug].vue
+++ pages/articles/[...slug].vue
@@ -1,11 +1,17 @@
+<script lang="ts" setup>
+const route = useRoute()
+const { data } = await await useAsyncData(() => queryCollection('content').path(route.path).first())
+
+useSeoMeta({
+  title: data.value?.title,
+  description: data.value?.description
+})
+</script>
+
 <template>
-  <article class="prose dark:prose-invert mx-auto">
-    <ContentDoc v-slot="{ doc }">
-      <div class="mb-8">
-        <h1>{{ doc.title }}</h1>
-        <p class="text-sm text-gray-600 dark:text-gray-400">{{ new Date(doc.date).toLocaleDateString() }}</p>
-      </div>
-      <ContentRenderer :value="doc" />
-    </ContentDoc>
+  <article class="prose dark:prose-invert mx-auto" v-if="data">
+    <p class="text-sm text-gray-600 dark:text-gray-400">{{ new Date(data?.meta.date).toLocaleDateString() }}</p>
+    <ContentRenderer :value="data" />
   </article>
+  <div v-else>Page not found</div>
 </template>
