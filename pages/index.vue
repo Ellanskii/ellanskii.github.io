02fe9660eec95298bacac224a3a0fb2cbe02cf88
Elijah Ellanski <ilya.ellanskiy@itms.tech>
2025-07-21 22:59:15 +0300

    fix(layout): index page buttons container

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,13 @@
       Hi! I'm a professional who loves to share knowledge and experiences.
       Feel free to check out my CV and articles.
     </p>
-  </div>
-  <div class="flex gap-4 mt-8">
-    <UButton to="/resume/">
-      View My CV
-    </UButton>
-    <UButton to="/articles/" variant="outline">
-      Read Articles
-    </UButton>
+    <div class="flex gap-4 mt-8 not-prose">
+      <UButton to="/resume/">
+        View My CV
+      </UButton>
+      <UButton to="/articles/" variant="outline">
+        Read Articles
+      </UButton>
+    </div>
   </div>
 </template>
