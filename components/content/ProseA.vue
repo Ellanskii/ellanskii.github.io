d24afa52a95be2071cbcc83b5d779a441d895f26
Elijah Ellanski <nameimprobable@gmail.com>
2026-06-10 15:26:11 +0300

    feat: add external link handling in ProseA component and update article writing guidelines

---
+++ components/content/ProseA.vue
@@ -0,0 +1,26 @@
+<template>
+  <NuxtLink
+    :href="href"
+    :target="isExternal ? '_blank' : target"
+    :rel="isExternal ? 'nofollow noopener noreferrer' : undefined"
+  >
+    <slot />
+    <UIcon v-if="isExternal" name="i-carbon-launch" class="inline-block align-text-top" />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  href: {
+    type: String,
+    default: '',
+  },
+  target: {
+    type: String,
+    default: undefined,
+    required: false,
+  },
+})
+
+const isExternal = computed(() => /^https?:\/\//.test(props.href))
+</script>
